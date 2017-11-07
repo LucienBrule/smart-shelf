4d38a12b16577620a31cc57dd5831e275bd7bcd3
Lucien Brule <lucienbrule@typhoon-335.dynamic.rpi.edu>
2017-11-06 20:30:48 -0500

    add devices and contents functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,79 +1,6 @@
 // Initialize Firebase
 firebase.initializeApp(config);
-// console.log(`Initialized firebase app: ${app.name}`);
-// var auth = app.auth();
-// var provider = new firebase.auth.GoogleAuthProvider();
-// var database = firebase.database();
-// var user = {}
-// var userURL;
-//
-// function writeUserData(userId, name, email, imageUrl) {
-// 	firebase.database().ref('users/' + userId).set({
-// 		username: name,
-// 		email: email,
-// 		profile_picture: imageUrl
-// 	});
-// }
-// lDKu7HgC8GQklFeJ21Fyim3jy2B3
-// firebase.auth().onAuthStateChanged(function(current_user) {
-// 	if (current_user) {
-// 		user = current_user;
-// 		// User is signed in.
-// 		console.log(`Successull login: ${user.displayName} <${user.email}> ID ${user.uid}`);
-// 		console.log(user);
-// 		current_user = user;
-//     userURL = 'users/' + user.uid;
-// 		$("#prof-pic").attr("src",user.photoURL);
-// 		$("#prof-name").text(user.displayName);
-// 		var dev0_ref = database.ref(`/users/${user.uid}/devices/0`);
-// 		dev0_ref.on("value",(snapshot)=>{
-// 			var data = snapshot.val();
-// 			console.log("got: " + data);
-// 			console.log(data);
-// 			$("#device-rep").text(JSON.stringify(data));
-// 		});
-// 	}else{
-// 		auth.signInWithRedirect(provider);
-// 		firebase.auth().getRedirectResult().then(function(result) {
-// 			if (result.credential) {
-// 				// This gives you a Google Access Token. You can use it to access the Google API.
-// 				var token = result.credential.accessToken;
-// 				// user = result.user;
-// 			}
-// 			// The signed-in user info.
-// 			// var user = result.user;
-// 		}).catch(function(error) {
-// 			// Handle Errors here.
-// 			var errorCode = error.code;
-// 			var errorMessage = error.message;
-// 			// The email of the user's account used.
-// 			var email = error.email;
-// 			// The firebase.auth.AuthCredential type that was used.
-// 			var credential = error.credential;
-// 			console.error(`Login error with ${email} of type ${errorCode}`);
-// 		});
-// 	}
-// });
-// function toggleOpenState() {
-// 	console.log("toggling shelf state");
-// 	var ds_ref = database.ref(`/users/${user.uid}/devices/0/door-state`);
-// 	ds_ref.once('value', (snapshot) => {
-// 		var data = snapshot.val()
-// 		console.log(data);
-// 		if (data == "closed") {
-// 			ds_ref.set("open");
-// 		} else if (data == "open") {
-// 			ds_ref.set("closed");
-// 		} else {
-// 			console.log("unkown transition state");
-// 		}
-// 		// if(data.)
-// 	})
-// }
 
-
-// Angular stuff
-// Define the `phonecatApp` module
 var iotApp = angular.module('iotApp', ["firebase"]);
 
 iotApp.service('deviceService', function() {
@@ -90,14 +17,7 @@ iotApp.service('deviceService', function() {
 	}
 });
 
-// iotApp.factory('userDevices', function($firebaseArray) {
-// 	var ref = firebase.database().ref(userURL + '/deviceNames');
-// 	console.log("user url is::: ");
-// 	console.log(userURL);
-//
-// 	var fa = $firebaseArray(ref);
-// 	return fa;
-// })
+
 
 iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebaseAuth, $firebaseArray, deviceService) {
 	$scope.mywords = "hello world";
@@ -106,6 +26,8 @@ iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebase
 	$scope.user = null;
 	$scope.ref = null;
 	$scope.obj = null;
+	$scope.itemsRef = null;
+	$scope.newdevicename = "new device";
 	$scope.auth = $firebaseAuth();
 	// $scope.currentdevice = ["thing1", "thing2"];
 	if (!$scope.devices[0]) {
@@ -116,7 +38,32 @@ iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebase
 
 	$scope.setActiveDevice = function(device) {
 		$scope.selected = device;
+		console.log("current device is: ");
 		console.log($scope.currentdevice);
+	}
+	$scope.toggleOpenState = function(){
+
+	}
+	$scope.addStoredItem = function(item){
+		console.log("running addStoredItem");
+		// var ref = firebase.database().ref(`users/${$scope.user.uid}/devices/0/contents`);
+		var newRef = $scope.itemsRef.push();
+		var val = prompt("Enter an item to add");
+		var num = prompt("How many, " + val + " ?");
+		console.log("gonna add this item: " + val);
+		var objToAdd = {"name":val,"amount":num}
+		newRef.set(objToAdd).then((data)=>{console.log("added a thing probbably")});
+		// console.log(list);
+	}
+	$scope.submitdevice = function(){
+		console.log("adding new device : " + $scope.newdevicename);
+		var newRef = $scope.devicesRef.push();
+		var objToAdd = {"name":$scope.newdevicename}
+		newRef.set(objToAdd).then((data)=>{console.log("added a new device probbably")});
+
+	}
+	$scope.toggleEnrollModal = function(){
+
 	}
 	$scope.toggleOpenState = function(){
 
@@ -138,6 +85,7 @@ iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebase
 		}
 
 	}
+
 	$scope.logout = function() {
 		if ($scope.user) {
 			$scope.auth.$signOut();
@@ -162,7 +110,12 @@ iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebase
 				});
         $scope.currentdevice = obj.devices[0];
 				$scope.selected = $scope.currentdevice.name;
-				$scope.devices = obj.devices;
+				$scope.devicesRef = firebase.database().ref(`users/${$scope.user.uid}/devices/`);
+				$scope.devicesArr = $firebaseArray($scope.devicesRef);
+				$scope.itemsRef =  firebase.database().ref(`users/${$scope.user.uid}/devices/0/contents`);
+				$scope.itemsArr = $firebaseArray($scope.itemsRef);
+
+
 			});
 
 		} else {
@@ -172,13 +125,4 @@ iotApp.controller('iotCtrl', function iotCtrl($scope, $firebaseObject, $firebase
 
 
 
-});
-
-iotApp.controller('modalCtrl', function modalCtrl($scope, deviceService) {
-	$scope.devicename = "new device";
-	$scope.submitdevice = function() {
-		console.log($scope.devicename);
-		// deviceService.addDevice($scope.devicename);
-
-	}
 });
